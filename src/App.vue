363699021ae4db528d22b0e7fc514e240fb6ecd9
Dama dany <damdany@gmail.com>
2022-08-07 23:39:17 +0300

    fin du home , debut de menu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex w-full h-full">
+    <header_comp></header_comp>
     <menu_nav v-if="this.$store.state.show_menu" />
-    <home/>
+    <home  />
   </div>
 </template>
 
 <script>
 import menu_nav from './components/MenuViews.vue'
+import header_comp from './components/HeaderView.vue'
 import home from './views/HomeView.vue'
 export default {
   components:{
     menu_nav,
     home,
+    header_comp
   }
 }
 </script>
@@ -24,11 +27,15 @@ export default {
 .bg_1{
   background:#233000;
 }
+
 .font_rufin{
   font-family: 'Rufina';
 }
 .font_lato{
-  font-family: 'Lato';
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.font_inter{
+  font-family: 'Inter';
 }
 #container_navigation{
   width: 100%;
